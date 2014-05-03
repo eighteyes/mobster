298201620a6df34e61a51056e2688a8d4234b539
Sean Canton <eighteyes@gmail.com>
2014-05-03 17:56:49 -0400

    Asd

--- index.js
+++ index.js
@@ -5,27 +5,27 @@ mongoose.connect('mongodb://localhost/mobster');
 var User = require('./models/user');
 var Location = require('./models/location');
 
-var app = require('http').createServer(handler)
-  , io = require('socket.io').listen(app)
-  , fs = require('fs')
+var app = require('http').createServer(handler),
+  io = require('socket.io').listen(app),
+  fs = require('fs')
 
-app.listen(1337);
+  app.listen(1337);
 
 
-function handler (req, res) {
+function handler(req, res) {
   fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
+    function(err, data) {
+      if (err) {
+        res.writeHead(500);
+        return res.end('Error loading index.html');
+      }
 
-    res.writeHead(200);
-    res.end(data);
-  });
+      res.writeHead(200);
+      res.end(data);
+    });
 }
 
-io.sockets.on('connection', function (socket) {
+io.sockets.on('connection', function(socket) {
   console.log('connection made');
 
   // Init > Client
@@ -34,47 +34,45 @@ io.sockets.on('connection', function (socket) {
   });
 
   // Client > Init
-  socket.on('init', function(data){
+  socket.on('init', function(data) {
     console.log('INIT DATA', data);
-    User.findOne({userId: data.userId}, function(err, user){
-      var loc = new Location({
-        user: user,
-        lat: data.lat,
-        lon: data.lon,
-        time: new Date().getTime()
-      });
-      loc.save( function(err, loc) {
-        if (err) console.log(err);
-        user.update({ location: loc }).exec();
-      });
-    });
+    updateUserLoc(data);
   });
 
-  socket.on('update', function(data){
-    console.log('INIT DATA', data);
-    User.findOne({userId: data.userId}, function(err, user){
-      Location.create({
-        user: user,
-        lat: data.lat,
-        lon: data.lon,
-        time: new Date().getTime()
-      }, function(err, loc){
-        if (err) console.log(err);
-        console.log(loc);
-      });
-
-    });
+  socket.on('update', function(data) {
+    console.log('UPDATE DATA', data);
+    updateUserLoc(data);
   });
 
-
-  socket.on('chat', function(data){
-    User.findOne({ userId: data.userId }, 'userName', function(err, user){
-      socket.broadcast.emit('chat', { msg: user.userName + " : " + data.msg });
-      socket.emit('chat', { msg: user.userName + " : " + data.msg });
+  socket.on('chat', function(data) {
+    User.findOne({
+      userId: data.userId
+    }, 'userName', function(err, user) {
+      socket.broadcast.emit('chat', {
+        msg: user.userName + " : " + data.msg
+      });
+      socket.emit('chat', {
+        msg: user.userName + " : " + data.msg
+      });
     });
   });
 });
 
-function makeInit(){
-  var payload = {};
+function updateUserLoc(data) {
+  User.findOne({
+    userId: data.userId
+  }, function(err, user) {
+    var loc = new Location({
+      user: user,
+      lat: data.lat,
+      lon: data.lon,
+      time: new Date().getTime()
+    });
+    loc.save(function(err, loc) {
+      if (err) console.log(err);
+      user.update({
+        location: loc
+      }).exec();
+    });
+  });
 }
